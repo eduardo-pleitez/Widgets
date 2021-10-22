f33e2275e543706a8b4735370786b25aee32998c
Eduardo Mejia <eduardo.77.1996@gmail.com>
2021-10-22 16:06:45 -0600

    accordion and tabs widget added

--- src/index.js
+++ src/index.js
@@ -20,3 +20,38 @@ function showSlides(n){
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+let j;
+let accordions = document.getElementsByClassName("accordion");
+for(j=0; j<accordions.length; j++){
+  accordions[j].addEventListener("click", function(){
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+
+    if(panel.style.display === "block"){
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block"
+    }
+  })
+}
+
+  let tab = document.getElementsByClassName("tabButton");
+  let k;
+  for(k=0; k<tab.length; k++){
+    tab[k].addEventListener("click", function(){
+      let l;
+      for(l=0; l<tab.length; l++){
+        tab[l].className = "tabButton";
+      }
+      this.className += " activeTab";
+      let m;
+      let tabContent = document.getElementsByClassName("tabContent");
+      for(m=0; m<tabContent.length; m++){
+        tabContent[m].style.display = "none";
+      }
+      if(this.value === "html"){tabContent[0].style.display = "block";}
+      if(this.value === "css"){tabContent[1].style.display = "block";}
+      if(this.value === "javascript"){tabContent[2].style.display = "block";}
+      if(this.value === "frameworks"){tabContent[3].style.display = "block";}
+    })}
